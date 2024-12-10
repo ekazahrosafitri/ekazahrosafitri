f28efa8037568151e1fce34d94ef79653ec300ae
ekazahrosafitri <ekazahrosafitri17@gmail.com>
2024-12-10 10:32:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+// Toggle & Responsive Navigation
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const navLists = document.querySelector("nav");
-
+  
     burger.addEventListener("click", () => {
-        navLists.classList.toggle("nav-active");
-        burger.classList.toggle("togle-burger");
+      // Toggle nav list and burger class
+      navLists.classList.toggle("nav-active");
+      burger.classList.toggle("toggle-burger");
     });
-}
-navSlide();
-
-window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName("form")){
-        form.reset();
+  };
+  
+  navSlide();
+  
+  // Clear form before unload
+  window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+      form.reset();
     }
-};
+  };
